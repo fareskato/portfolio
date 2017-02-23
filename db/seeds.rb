d05f78aa4f38c12da520e9b0d397544b3b770149
Fares Kato <fareskato@gmail.com>
2017-02-23 22:09:17 +0300

    create seeder for sample data(blogs, skills an portfolios)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+#  CREATE SEED FOR BLOG
+10.times do |blog|
+  Blog.create!(
+    title: "My blog post #{blog}",
+    body: "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum"
+  )
+end
+
+puts '10 blogs created'
+
+#  CREATE SEED FOR SKILLS
+5.times do |skill|
+  Skill.create!(
+    title: "Rails #{skill}",
+    percent_utilized: 15
+  )
+end
+
+puts '5 skills created'
+
+#  CREATE SEED FOR PORTFOLIO
+9.times do |portfolio|
+  Portfolio.create!(
+    title: "Portfolio title #{portfolio}",
+    subtitle: 'This is subtitle',
+    body: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout' ,
+    main_image: "http://placehold.it/600x400" ,
+    thumb_image: "http://placehold.it/350x200"
+  )
+end
+
+puts '9 portfolios created'
