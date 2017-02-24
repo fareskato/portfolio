a8c8d06b79153a3c2edf9c865f9c8dc5eab04fc0
Fares Kato <fareskato@gmail.com>
2017-02-24 14:30:34 +0300

    customize resource routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,22 @@
 Rails.application.routes.draw do
   # Root route (Home page)
-  root :to => 'pages#home'
+  root to: 'pages#home'
 
   # Portfolio routes
-  resources :portfolios
+  # customize resource specific route
+  resources :portfolios , except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+  # get 'portfolio/:id/edit', to: 'portfolios#edit', as: 'portfolio_edit'
+
+
 
   # Blogs routes
   resources :blogs
 
   # static pages routes
-  # get 'about', :to => 'pages#about'
-  get 'about-me', :to => 'pages#about'
-  get 'contact', :to => 'pages#contact'
+  # get 'about', to: 'pages#about'
+  get 'about-me', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
 
 
 
