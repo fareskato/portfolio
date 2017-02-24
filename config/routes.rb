8bb478371dac497237f23247b32f72a5e1a37dc6
Fares Kato <fareskato@gmail.com>
2017-02-24 14:14:41 +0300

    routes modifications(HP, contact and about)

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
+  # Root route (Home page)
+  root :to => 'pages#home'
+
+  # Portfolio routes
   resources :portfolios
-  get 'pages/home'
 
-  get 'pages/about'
-
-  get 'pages/contact'
-
+  # Blogs routes
   resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # static pages routes
+  # get 'about', :to => 'pages#about'
+  get 'about-me', :to => 'pages#about'
+  get 'contact', :to => 'pages#contact'
+
+
+
 end
