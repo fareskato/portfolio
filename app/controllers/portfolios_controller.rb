bb43f13de69f8a59a6f87d51024eaf34628d90c0
Fares Kato <fareskato@gmail.com>
2017-02-24 11:55:24 +0300

    create new and create portfolio

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,6 +1,31 @@
 class PortfoliosController < ApplicationController
+  #  all portfolios
   def index
     @portfolio_items = Portfolio.all
+  end
+
+  #  Create new portfolio
+  def new
+    @portfolio_item = Portfolio.new
+  end
+
+  def create
+    @portfolio_item = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Your Portfolio is live.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  #  Private methods
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :subtitle, :body)
   end
 
 end
