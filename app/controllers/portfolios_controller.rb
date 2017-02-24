8eee302061dd3a2a4217fdefc3693b28d183dea0
Fares Kato <fareskato@gmail.com>
2017-02-24 12:20:38 +0300

    edit and update functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,10 +1,10 @@
 class PortfoliosController < ApplicationController
-  #  all portfolios
+  ########## ALL PORTFOLIOS ##########
   def index
     @portfolio_items = Portfolio.all
   end
 
-  #  Create new portfolio
+  ########## CREATE PORTFOLIO (new , create) ##########
   def new
     @portfolio_item = Portfolio.new
   end
@@ -21,7 +21,24 @@ class PortfoliosController < ApplicationController
     end
   end
 
-  #  Private methods
+  ########## UPDATE PORTFOLIO (edit, update)##########
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'portfolio was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
+
+  ########## PRIVATE METHODS ##########
   private
 
   def portfolio_params
