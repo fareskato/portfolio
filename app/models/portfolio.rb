44a8672d811b5c6b2adc155ab6ede3216438cec8
fares kato <fareskato@gmail.com>
2017-04-29 19:13:25 +0300

    create concern

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,4 +1,7 @@
 class Portfolio < ApplicationRecord
+#  invoke the concern
+	include Placeholder
+
   
   ##### Valodation
   validates_presence_of :title, :body, :main_image, :thumb_image
@@ -14,10 +17,10 @@ class Portfolio < ApplicationRecord
 
   ##### Create default value using rails callbacks
   after_initialize :set_defaults
-
+  # use Placeholder concern
   def set_defaults
-  	self.main_image ||= "http://placehold.it/600x400"
-  	self.thumb_image ||= "http://placehold.it/350x200"
+  	self.main_image ||= Placeholder.image_generator(height:600, width:400)
+  	self.thumb_image ||= Placeholder.image_generator(height:350, width:200)
   end
 
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,7 +1,16 @@
 class Skill < ApplicationRecord
-#   validations
+#  invoke the concern
+include Placeholder
+
+#####  validations
   validates_presence_of :title, :percent_utilized
 
-  # Relation ships
+ ##### Create default value using rails callbacks
+  after_initialize :set_defaults
+  # use Placeholder concern
+  def set_defaults
+  	self.main_image ||= Placeholder.image_generator(height:250, width:250)
+  end
+  
 
 end
